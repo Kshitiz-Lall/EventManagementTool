f1e39fa9d426c768eac60d415da63b6e3e17613a
Ankit Sengar <Ankit.sengar@genzeon.com>
2023-06-21 15:59:14 +0530

    update upto axios

--- client-app/src/features/activities/dashboard/activityDashboard.tsx
+++ client-app/src/features/activities/dashboard/activityDashboard.tsx
@@ -15,11 +15,12 @@ interface Props {
     closeForm: () => void;
     createOrEdit: (activity: Activity) =>void
     deleteActivity: (id:string) => void;
+    submitting: boolean;
 }
 
 
 export default function ActivityDashboard({activities, selectedActivity,deleteActivity,
-selectActivity,cancelSelectActivity, editMode, openForm, closeForm,createOrEdit}: Props)
+selectActivity,cancelSelectActivity, editMode, openForm, closeForm,createOrEdit,submitting}: Props)
 {
     return(
         <Grid>
@@ -27,6 +28,7 @@ selectActivity,cancelSelectActivity, editMode, openForm, closeForm,createOrEdit}
                <ActivityList activities={activities} 
                              selectActivity={selectActivity}
                              deleteActivity={deleteActivity}
+                             submitting={submitting}
                 />
             </Grid.Column>
             
@@ -38,7 +40,12 @@ selectActivity,cancelSelectActivity, editMode, openForm, closeForm,createOrEdit}
             openForm={openForm}
             />}
             {editMode &&
-            <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit}/>}
+            <ActivityForm 
+            closeForm={closeForm} 
+            activity={selectedActivity} 
+            createOrEdit={createOrEdit}
+            submitting={submitting}
+            />}
             </Grid.Column>
         </Grid>
     )
