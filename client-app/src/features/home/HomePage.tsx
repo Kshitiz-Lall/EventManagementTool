35947a721042bc9836189d2701c645d8e0115d04
vivek <vivek.kumar@genzeon.com>
2023-06-26 14:46:12 +0530

    Done with Styling the home page

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,13 +1,24 @@
 import { Link } from "react-router-dom";
-import { Container } from "semantic-ui-react";
+import { Button, Container, Header, Image, Segment } from "semantic-ui-react";
 
 export default function HomePage() {
   return (
-    <Container style={{ marginTop: "7em" }}>
-      <h1>Home Page</h1>
-      <h3>
-        Go to <Link to="/activities">Event Management App</Link>
-      </h3>
-    </Container>
+    <Segment inverted textAlign="center" vertical className="masthead">
+      <Container text>
+        <Header as="h1" inverted>
+          <Image
+            size="massive"
+            src="/assets/genzeon-logo2.png"
+            alt="logo"
+            style={{ marginBottom: 12 }}
+          />
+          GENZEON
+        </Header>
+        <Header as="h2" inverted content="Welcome To Event Management App " />
+        <Button as={Link} to="/activities" size="huge" inverted>
+          Take me to the Events!
+        </Button>
+      </Container>
+    </Segment>
   );
 }
