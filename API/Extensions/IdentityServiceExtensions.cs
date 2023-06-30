9d1b0990daf8ca19681b9b2d6b9832349b6cd088
KshitizLall <kszzz96@gmail.com>
2023-06-30 14:39:32 +0530

    Updated the Table using EF

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -1,11 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using API.Services;
 using Domain;
+using Infrastructure.Security;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.IdentityModel.Tokens;
 using Persistence;
 
@@ -14,11 +12,11 @@ namespace API.Extensions
   public static class IdentityServiceExtensions
   {
     public static IServiceCollection AddIdentityServices(this IServiceCollection services,
-     IConfiguration config)
+        IConfiguration config)
     {
       services.AddIdentityCore<AppUser>(opt =>
       {
-        // opt.Password.RequireNonAlphanumeric = false;
+        opt.Password.RequireNonAlphanumeric = false;
         opt.User.RequireUniqueEmail = true;
       })
       .AddEntityFrameworkStores<DataContext>();
@@ -26,16 +24,26 @@ namespace API.Extensions
       var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
 
       services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-      .AddJwtBearer(opt =>
+          .AddJwtBearer(opt =>
+          {
+            opt.TokenValidationParameters = new TokenValidationParameters
+            {
+              ValidateIssuerSigningKey = true,
+              IssuerSigningKey = key,
+              ValidateIssuer = false,
+              ValidateAudience = false
+            };
+          });
+
+      services.AddAuthorization(opt =>
       {
-        opt.TokenValidationParameters = new TokenValidationParameters
-        {
-          ValidateIssuerSigningKey = true,
-          IssuerSigningKey = key,
-          ValidateIssuer = false,
-          ValidateAudience = false
-        };
+        opt.AddPolicy("IsActivityHost", policy =>
+              {
+            policy.Requirements.Add(new IsHostRequirement());
+          });
       });
+
+      services.AddTransient<IAuthorizationHandler, IsHostRequirementHandler>();
       services.AddScoped<TokenService>();
 
       return services;
