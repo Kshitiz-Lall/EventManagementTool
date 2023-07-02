8273c58d61e9bec9abe73e0f63b8bedd409cc283
KshitizLall <kszzz96@gmail.com>
2023-07-02 19:41:09 +0530

    Stable Version

--- client-app/src/features/profiles/ProfileContent.tsx
+++ client-app/src/features/profiles/ProfileContent.tsx
@@ -2,6 +2,8 @@ import { observer } from "mobx-react-lite";
 import { Tab } from "semantic-ui-react";
 import { Profile } from "../../app/models/profile";
 import { useStore } from "../../app/stores/store";
+import ProfileAbout from "./ProfileAbout";
+import ProfileActivities from "./ProfileActivities";
 import ProfileFollowings from "./ProfileFollowings";
 import ProfilePhotos from "./ProfilePhotos";
 
@@ -13,8 +15,9 @@ export default observer(function ProfileContent({ profile }: Props) {
   const { profileStore } = useStore();
 
   const panes = [
+    { menuItem: "About", render: () => <ProfileAbout /> },
     { menuItem: "Photos", render: () => <ProfilePhotos profile={profile} /> },
-    { menuItem: "Events", render: () => <Tab.Pane>Events Content</Tab.Pane> },
+    { menuItem: "Events", render: () => <ProfileActivities /> },
     { menuItem: "Followers", render: () => <ProfileFollowings /> },
     { menuItem: "Following", render: () => <ProfileFollowings /> },
   ];
