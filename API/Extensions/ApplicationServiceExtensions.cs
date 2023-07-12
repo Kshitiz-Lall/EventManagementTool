8eed0568647456cc7d586cdebcff0886a54aa675
KshitizLall <kszzz96@gmail.com>
2023-07-12 15:52:37 +0530

    certain changes in files

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -59,12 +59,12 @@ namespace API.Extensions
       {
         opt.AddPolicy("CorsPolicy", policy =>
               {
-            policy
-                      .AllowAnyMethod()
-                      .AllowAnyHeader()
-                      .AllowCredentials()
-                      .WithOrigins("http://localhost:3000", "https://localhost:3000");
-          });
+                policy
+                          .AllowAnyMethod()
+                          .AllowAnyHeader()
+                          .AllowCredentials()
+                          .WithOrigins("http://localhost:3000", "https://localhost:3000");
+              });
       });
       services.AddMediatR(typeof(List.Handler));
       services.AddAutoMapper(typeof(MappingProfiles).Assembly);
